09373b5ea6e1f0ca53d9f159f3d542dd35dcec17
James Puro <james.louie.puro@gmail.com>
2018-12-22 04:45:11 +0800

    Fonts

---
+++ pages/about/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about-page">
+    <h1>This is my absolutely fanstastic "About Me" Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi tenetur magni quam ad dolores quod sequi hic rem praesentium ea cum quia fugiat aperiam incidunt sed culpa, vel esse autem!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus optio magni odit. Dolorem dicta facilis voluptas. Explicabo praesentium quis tempore dolores! Corporis, illum? Corrupti dignissimos perspiciatis aut ut accusamus? Vero!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus unde autem voluptates soluta voluptatem modi, nam vitae minima quisquam veritatis reiciendis sit iure fugiat numquam blanditiis, facere expedita incidunt illo.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus harum repellendus magni sit porro ea sequi natus adipisci ullam, accusamus earum, impedit, voluptatem provident culpa aliquid quaerat numquam neque fugit!</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
